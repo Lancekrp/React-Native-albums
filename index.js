e5b38470cd345b5d3d7585110d8043a788290215
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-13 19:29:46 +0800

    making a reusable header

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 const App = () => (
-  <Header />
+  <Header headerText="Albums" />
 );
 
 AppRegistry.registerComponent('albums', () => App);
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: 'blue'
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    paddingTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
