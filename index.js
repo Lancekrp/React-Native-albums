dc1df2160a07ee11b85c704e1dea5843264e7651
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-13 18:57:51 +0800

    consuming file exports

--- index.js
+++ index.js
@@ -9,10 +9,11 @@
 // AppRegistry.registerComponent(appName, () => App);
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 const App = () => (
-  <Text>Some text</Text>
+  <Header />
 );
 
 AppRegistry.registerComponent('albums', () => App);
