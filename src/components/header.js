103961578a7c1b0714ca6f090a11341196c3c27c
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-13 19:09:08 +0800

    more component styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
   return (
-    <Text style={textStyle}>Albums</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
   );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: 'blue'
+  },
   textStyle: {
     fontSize: 20
   }
