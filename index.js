3419e79ad9ae4bd3cb3eaedad583b9586151e52c
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-13 18:26:25 +0800

    creating a component with jsx

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+import React from 'react';
+import ReactNAtive from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some text</Text>
+  );
+};
